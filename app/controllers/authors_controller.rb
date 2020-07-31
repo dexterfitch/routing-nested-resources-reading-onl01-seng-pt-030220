b5273e5a499cf46df143ceb52df85f79e5a27e60
Dexter Fitch <fitch.dexter@gmail.com>
2020-07-31 21:37:17 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,13 +1,18 @@
 class AuthorsController < ApplicationController
 
   def show
-    @author = Author.find(params[:id])
+    @author = get_author_by_id
   end
 
   def posts_index
   end
 
   def posts
+  end
+
+private
+  def get_author_by_id
+    Author.find(params[:id])
   end
 
 end
