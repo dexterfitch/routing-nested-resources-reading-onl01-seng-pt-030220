2ff997b1415e5cec9938e569a558060741bfae64
Dexter Fitch <fitch.dexter@gmail.com>
2020-07-31 21:47:09 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,18 +4,6 @@ class AuthorsController < ApplicationController
     @author = get_author_by_id
   end
 
-  def posts_index
-    @author = get_author_by_id
-    @posts = @author.posts
-    render template: 'posts/index'
-  end
-
-  def posts
-    @author = get_author_by_id
-    @post = Post.find(params[:post_id])
-    render template: 'posts/show'
-  end
-
 private
   def get_author_by_id
     Author.find(params[:id])
